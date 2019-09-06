61f932dae6c0ae5123a2250be6d2bb707b483da1
Jason Sonnichsen <Harliquinn@gmail.com>
2019-09-06 12:45:59 -0400

    Testing implemented. MVP met

--- client/src/components/CardList.js
+++ client/src/components/CardList.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {useDark} from '../hooks/useDark.js';
 
 const CardList = (props) => {
+    const [darkMode, setDarkMode] = useDark(false);
     return (
         <div className="cardList">
+            <button type="button" onClick={()=>{setDarkMode(!darkMode)}}>Use Dark Mode</button>
             {props.data.map(item=>{
-                return <h2>{item.name}</h2>
+                return <h2 key={item.id}>{item.name}</h2>
             })}
         </div>
       );
